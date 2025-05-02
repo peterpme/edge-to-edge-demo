91a5d5479681eb8b0471f9519de95a98c05c89ef
Peter Piekarczyk <peterpme@users.noreply.github.com>
2025-05-02 10:12:17 -0500

    edge to edge disabled

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-cont
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Edge to Edge Enabled. New Arch Disabled</Text>
+      <Text>Edge to Edge DISABLED. New Arch Disabled</Text>
     </View>
   );
 }
