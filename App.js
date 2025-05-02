08fd174f027796fdfc7604db8a825e8aa74338fc
Peter Piekarczyk <peterpme@users.noreply.github.com>
2025-05-02 10:01:45 -0500

    init example

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context'
 
-export default function App() {
+function NavLayout({ children }) {
+  const insets = useSafeAreaInsets();
+  return <View style={{ flex: 1, paddingTop: insets.top }}>{children}</View>;
+}
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'yellow' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const NativeStack = createNativeStackNavigator({
+  screens: {
+    Home: {
+      screen: HomeScreen,
+      options: {
+        title: "NativeStack",
+      },
+    }
+  }
+})
+
+const Tabs = createBottomTabNavigator({
+  layout: NavLayout,
+  screens: {
+    Home: {
+      screen: NativeStack,
+      options: {
+        title: "Tabs",
+      },
+    }
+  }
+})
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    BottomTabs: Tabs,
   },
 });
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
+  )
+}
+
